612bbd9b716d88110dd80daa18e0627c5caac1d8
fychen2024-cyber <fychen2024@gmail.com>
2026-09-14 16:43:35 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// 桌球同好地圖全域設定
+window.APP_CONFIG = {
+  // Google Apps Script 部署後的網頁應用程式網址 (Web App URL)
+  // 請參考「Google試算表後端建置教學.md」取得網址後貼於此處
+  // 例如: "https://script.google.com/macros/s/AKfycbx.../exec"
+  API_URL: "",
+  
+  // LINE 社群邀請連結
+  LINE_COMMUNITY_URL: "https://line.me/ti/g2/MLL932ZYs6ffmsQQ3zChMOY8DhusjZfaP_GP0w?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
+};
